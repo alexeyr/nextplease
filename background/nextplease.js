22ab268c8edd1aa83f53bfae8e10d972ff403bfd
Alexey Romanov <alexey.v.romanov@gmail.com>
2018-02-07 01:04:11 +0300

    Add key shortcuts

---
+++ background/nextplease.js
@@ -0,0 +1,22 @@
+(function () {
+    /**
+      * Listen for commands.
+      */
+    browser.commands.onCommand.addListener((direction) => {
+        browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+            var active_tab = tabs[0];
+
+            browser.tabs.sendMessage(active_tab.id, {
+                command: e.target.id
+            });
+        }).catch(reportExecuteScriptError);
+    });
+
+    /**
+     * There was an error executing the script.
+     * Display the popup's error message, and hide the normal UI.
+     */
+    function reportExecuteScriptError(error) {
+        console.error(`Failed to execute NextPlease script: ${error.message}`);
+    }
+})();
